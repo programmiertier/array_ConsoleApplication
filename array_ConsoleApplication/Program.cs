3c1e11e0ba6dc2266beb01d555b5eea96da5e803
programmiertier <zimmermannveronique@protonmail.com>
2017-10-11 09:04:39 +0200

    array
    array of strings, array of objects.
    Ausgabe mit WriteLine("{0}\n{1}", warenkorb);
    oooder als Schleife

--- array_ConsoleApplication/Program.cs
+++ array_ConsoleApplication/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace array_ConsoleApplication
 {
@@ -10,6 +7,36 @@ namespace array_ConsoleApplication
     {
         static void Main(string[] args)
         {
+            // type der im array enthaltenen Elemente,
+                    // name des array
+                                // object anlegt
+                                            // reserviert 5 Plätze im Speicher
+            string[] warenkorb = new string[7];
+            warenkorb[0] = "200 gr. Tüte Gummibären";
+            warenkorb[1] = "1 Pfund Butter";
+            warenkorb[2] = "1 Paprika";
+            warenkorb[3] = "1 kg Kaffee";
+            
+            // WriteLine("Wir wollen kaufen:\n{0}\n{2}\n{3}\n{1}\n", warenkorb);
+            // mein WriteLine erspart folgende Schleife
+            for(int artikel = 0; artikel < warenkorb.Length; artikel++) // 7 maximal, angegeben 4, lässt für die fehlenden eine Zeile
+            {
+                WriteLine("| {0} |", warenkorb[artikel]);
+            }
+
+
+            object[] universal = new object[10];
+            universal[0] = "200 gr. Tüte Gummibären";
+            universal[1] = 555;
+            universal[2] = 'a';
+
+            // WriteLine("{0}\n{1}", universal);
+            for(int artikel = 0; artikel < universal.Length; artikel++)
+            {
+                WriteLine("| {0} |", universal[artikel]);
+            }
+
+            ReadLine();
         }
     }
 }
